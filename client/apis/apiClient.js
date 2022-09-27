e2ac026e19e1232d71868b69cdcbc014bc4b13a4
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-09-28 09:28:11 +1300

    delete db folder

--- client/apis/apiClient.js
+++ client/apis/apiClient.js
@@ -1,5 +1,4 @@
 import request from 'superagent'
-const rootUrl = '/api/v1'
 
 export function getPokemon(limit) {
   return request
@@ -16,24 +15,3 @@ export function getPokeInfo(name) {
       return res.body
     })
 }
-
-// export function saveTeam(team) {
-//   console.log('api', team)
-//   return request
-//     .post(rootUrl + '/teamHistory')
-//     .send({ team })
-//     .then((res) => {
-//       return res.body
-//     })
-// }
-
-// export function deleteEntireTeamHistory() {
-//   return request.delete(rootUrl + '/teamHistory').then((res) => {
-//     return res.body
-//   })
-// }
-// export function getAllTeamHistory() {
-//   return request.get(rootUrl + '/teamHistory').then((res) => {
-//     return res.body
-//   })
-// }
--- server/db/seeds/teamHistory.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('teamHistory')
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('teamHistory').insert([{ team: 'bulby' }])
-    })
-}
